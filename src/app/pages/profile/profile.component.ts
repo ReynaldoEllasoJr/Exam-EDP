f686c6b972850c4d2b0ec31393cb3a457c2f04cc
ReynaldoEllasoJr <you@example.com>
2021-10-27 03:27:55 +0800

    first commit

---
+++ src/app/pages/profile/profile.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.scss']
+})
+export class ProfileComponent implements OnInit {
+  @Input() isClickLike:boolean=false;
+  
+  
+  
+  @Input() isClickHeart:boolean=false;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
